1e378c2ea3bbb3ea8d76267c52d6e1130a8d6dfb
ysl0628 <yihsinlan@gmail.com>
2022-08-09 22:24:21 +0800

    feat: implement api url config - 建立 api 網址動態變數

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,3 +1,4 @@
+import { serverUrl } from "./utlis/config";
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
@@ -6,7 +7,7 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api/upload", {
       // http://8rwbc7.natappfree.cc  地址只是实例，实际地址为项目为基准
-      target: "http://localhost:1337",
+      target: `${serverUrl}`,
       //跨域时一般设置该值为 true
       changeOrigin: true,
       //重写接口路由

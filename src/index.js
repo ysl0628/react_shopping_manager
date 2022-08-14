9bf3d8d002d058e7f3030ff32692cd30aad3ed58
ysl0628 <yihsinlan@gmail.com>
2022-08-14 16:43:03 +0800

    revert: replace serverUrl url

--- src/index.js
+++ src/index.js
@@ -8,15 +8,10 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 
-let base_name = "";
-process.env.NODE_ENV === "development"
-  ? (base_name = "/")
-  : (base_name = "/react_shopping_manager");
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Router basename={base_name}>
+    <Router>
       <App />
     </Router>
   </Provider>
